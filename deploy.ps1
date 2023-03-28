b1ff6a217a6a9a26c89348530997d72b1eccc86a
rafaelsilvaipb <39627828+rafaelsilvaipb@users.noreply.github.com>
2023-03-28 18:45:13 -0300

    Delete deploy.ps1

--- deploy.ps1
+++
@@ -1,8 +0,0 @@
-& minikube start
-& kubectl delete regcred
-& kubectl create secret docker-registry regcred --docker-server=661984127532.dkr.ecr.us-east-1.amazonaws.com --docker-username=aws --docker-password=$(aws ecr get-login-password) --docker-email=rafaelsilva.ipbaws@gmail.com
-& kubectl apply -f .\ingress.yaml
-& kubectl expose deployment kroton-deployment --type=NodePort
-& minikube service kroton-deployment --url
-
--noexit
